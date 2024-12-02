a26233ba4a8d94058c425027433b54981d29037b
Ahmedelj <ahmedeljannoun@gmail.com>
2024-12-01 21:53:47 -0500

    styling and structuring the app

--- SRC/client/src/components/pages/Home.jsx
+++ SRC/client/src/components/pages/Home.jsx
@@ -5,6 +5,11 @@ export default function Home() {
     <div className="container">
       <div id="homepage">
         <h1>Welcome to the Home Page</h1>
+        <p>This application allows you to efficiently manage your business
+          operations. You can view and manage your products, keep track of
+          inventory and restocking needs, manage user accounts and their orders,
+          and view customer orders seamlessly. Use the navigation links
+          above to access the different sections of the platform.</p>
         <div className="links">
         </div>
       </div>
